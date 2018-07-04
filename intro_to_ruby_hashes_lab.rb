f38718ccfa547cf4e36b7b35820c022189de6383
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-04 18:54:34 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,6 +18,8 @@ def monopoly_with_second_tier
   monopoly[:railroads][:pieces] = 4
   monopoly[:railroads][:names] = {} 
   monopoly[:railroads][:rent_in_dollars] = {}
+  monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  
   {reading_railroad: {}}, : {one_piece_owned: 25, two_piece_owned: 50, three_piece_owned: 100, four_piece_owned: 200}}
 end
 
