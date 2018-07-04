1967762185157d45448ed53d40ad72fa2ac8f3ab
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-04 19:08:06 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,9 +44,12 @@ def monopoly_with_fourth_tier
 	  railroads: {pieces: 4, names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline: {}}, 
 	  rent_in_dollars: {
 	    one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200
-	    }
-	    
-	  }}
+	    }}}
+	 monopoly[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
+	 monopoly[:railroads][:names][:b_and_o_railroad]["mortgage_value"] = "$400"
+	 monopoly[:railroads][:names][:shortline]["mortgage_value"] = "$800"
+	 
+	 monopoly
 end
 
 
